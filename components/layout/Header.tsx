52c21fe21eb8b72ef7110d4f5c38abdab9b4b29d
Koushik Saha <koushik.saha666@gmail.com>
2026-01-18 13:02:19 -0800

    fixed header

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -9,7 +9,7 @@ export default function Header() {
 
   return (
     <header className="bg-white shadow-sm sticky top-0 z-50 border-b border-gray-200">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16 md:h-20">
           {/* Logo */}
           <Link href="/" className="flex items-center flex-shrink-0">
